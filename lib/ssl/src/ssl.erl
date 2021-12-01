9a2cba889a7293850992f4d34f08228107b04708
zhouzb <zhouzb@emqx.io>
2021-12-01 22:16:02 +0800

    feat(ssl): no warning log when using verify_none

--- lib/ssl/src/ssl.erl
+++ lib/ssl/src/ssl.erl
@@ -1539,9 +1539,9 @@ handle_options(Transport, Socket, Opts0, Role, Host) ->
                           #{role => Role,
                             host => Host,
                             rules => ?RULES}),
-    
-    maybe_client_warn_no_verify(SslOpts2, Role),
+
     SslOpts = maps:without([warn_verify_none], SslOpts2),
+
     %% Handle special options
     {Sock, Emulated} = emulated_options(Transport, Socket, Protocol, SockOpts0),
     ConnetionCb = connection_cb(Protocol),
@@ -2873,15 +2873,6 @@ add_filter(undefined, Filters) ->
     Filters;
 add_filter(Filter, Filters) ->
     [Filter | Filters].
-
-maybe_client_warn_no_verify(#{verify := verify_none,
-                             warn_verify_none := true,
-                             log_level := LogLevel}, client) ->
-            ssl_logger:log(warning, LogLevel, #{description => "Authenticity is not established by certificate path validation",
-                                                reason => "Option {verify, verify_peer} and cacertfile/cacerts is missing"}, ?LOCATION);
-maybe_client_warn_no_verify(_,_) ->
-    %% Warning not needed. Note client certificate validation is optional in TLS
-    ok.
 
 unambiguous_path(Value) ->
     AbsName = filename:absname(Value),
