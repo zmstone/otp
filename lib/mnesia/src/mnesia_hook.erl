1be5484b903ca189752a09c45f366c13f769a641
Thales Macedo Garitezi <thalesmg@gmail.com>
2022-01-21 16:31:42 -0300

    style: change tabs to spaces

--- lib/mnesia/src/mnesia_hook.erl
+++ lib/mnesia/src/mnesia_hook.erl
@@ -23,20 +23,20 @@
 -include("mnesia.hrl").
 
 -export([
-	 register_hook/2,
-	 do_post_commit/2
-	]).
+         register_hook/2,
+         do_post_commit/2
+        ]).
 
 -define(hook(NAME), {mnesia_hook, NAME}).
 
 -type post_commit_hook_data() ::
-	#{ node => node()
-	 , ram_copies => list()
-	 , disc_copies => list()
-	 , disc_only_copies => list()
-	 , ext => list()
-	 , schema_ops => list()
-	 }.
+        #{ node => node()
+         , ram_copies => list()
+         , disc_copies => list()
+         , disc_only_copies => list()
+         , ext => list()
+         , schema_ops => list()
+         }.
 
 -type post_commit_hook() :: fun((_Tid, post_commit_hook_data()) -> ok).
 
@@ -50,25 +50,25 @@ register_hook(_, _) ->
 do_post_commit(Tid, Commit) ->
     case persistent_term:get(?hook(post_commit), undefined) of
         undefined ->
-	    ok;
-      Fun ->
-   	    #commit{ node = Node
-	           , ram_copies = Ram
-		   , disc_copies = Disc
-		   , disc_only_copies = DiscOnly
-		   , ext = Ext
-		   , schema_ops = SchemaOps
-		   } = Commit,
-	    CommitData = #{ node => Node
-			  , ram_copies => Ram
-			  , disc_copies => Disc
-			  , disc_only_copies => DiscOnly
-			  , ext => Ext
-			  , schema_ops => SchemaOps
-			  },
+            ok;
+        Fun ->
+            #commit{ node = Node
+                   , ram_copies = Ram
+                   , disc_copies = Disc
+                   , disc_only_copies = DiscOnly
+                   , ext = Ext
+                   , schema_ops = SchemaOps
+                   } = Commit,
+            CommitData = #{ node => Node
+                          , ram_copies => Ram
+                          , disc_copies => Disc
+                          , disc_only_copies => DiscOnly
+                          , ext => Ext
+                          , schema_ops => SchemaOps
+                          },
             try Fun(Tid, CommitData)
             catch EC:Err:Stack ->
-		mnesia_lib:error("Mnesia post_commit hook failed: ~p:~p~nStacktrace:~p~n", [EC, Err, Stack])
-	    end,
-	    ok
+                    mnesia_lib:error("Mnesia post_commit hook failed: ~p:~p~nStacktrace:~p~n", [EC, Err, Stack])
+            end,
+            ok
     end.
